49770de2dd671e85b62dcdd1aaf0465a71522dfb
dailykoder <112426800+dailykoder@users.noreply.github.com>
2025-04-24 05:48:42 -0700

    deploying to gh pages

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  output: "export",
   content: [
     // add this
+
     "./@/components/**/*.{ts,tsx}",
   ],
 };
